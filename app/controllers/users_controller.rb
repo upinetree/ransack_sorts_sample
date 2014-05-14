1a38cb1b41f2d68cfe7d86862a0db1d04ca1808d
upinetree <t.matsumoto@everyleaf.com>
2014-05-15 00:50:03 +0900

    Enable kaminari

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def index
     @search = User.search(params[:q])
-    @users = @search.result
+    @users = @search.result.page(params[:page])
   end
 
   def show
