8b081dcfc1521eb03bdc20dfd970769218b13813
upinetree <t.matsumoto@everyleaf.com>
2014-05-15 00:22:17 +0900

    Enable ransack sort_link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    @users = User.all
+    @search = User.search(params[:q])
+    @users = @search.result
   end
 
   def show
